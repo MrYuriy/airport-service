7f431851716583bba1168882fd5048c9206db71f
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-08-25 18:45:04 +0300

    Add Airport Route Crew AirplaneType Airplane Flight Ticket Order models

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,127 @@
 from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import transaction
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="source_routes"
+    )
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="destination_routes"
+    )
+    distance = models.IntegerField()
+
+    def clean(self):
+        if self.source == self.destination:
+            raise ValidationError("Source and destination cannot be the same.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Route from {self.source} to {self.destination}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["source", "destination"], name="unique_route"
+            )
+        ]
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    air_plane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    @property
+    def duration(self):
+        return self.arrival_time - self.departure_time
+
+    def clean(self):
+        if self.arrival_time <= self.departure_time:
+            raise ValidationError("Arrival time must be later than departure time.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return (
+            f"{self.route} {self.airplane} {self.departure_time} - {self.arrival_time}"
+        )
+
+
+class Ticket(models.Model):
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name="tickets")
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    @staticmethod
+    def validate_ticket(row, seat, airplane):
+        if not (1 <= row <= airplane.rows and 1 <= seat <= airplane.seats_in_row):
+            raise ValidationError(
+                f"Invalid ticket: row must be in range (1, {airplane.rows}), seat must be in range (1, {airplane.seats_in_row})"
+            )
+
+    def clean(self):
+        self.validate_ticket(self.row, self.seat, self.flight.airplane)
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{str(self.flight)} (row: {self.row}, seat: {self.seat})"
+
+    class Meta:
+        unique_together = ("flight", "row", "seat")
+        ordering = ["row", "seat"]
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
